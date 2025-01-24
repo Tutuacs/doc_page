2ec6759ffbdf65453dedc9bf4e859c8aaeb4e4c6
Arthur_Silva <arthursilva.rs1@gmail.com>
2025-01-24 13:39:02 -0300

    :boom: fix: comment Thumb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Thumb from "@/components/Thumb";
+// import Thumb from "@/components/Thumb";
 import Image from "next/image";
 
 const geistSans = Geist({
