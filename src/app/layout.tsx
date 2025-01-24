efd846abe0b64baf665236f5c36ee447d7153a12
Arthur_Silva <arthursilva.rs1@gmail.com>
2025-01-24 11:39:05 -0300

    :sparkles: feat: change ImageThumb location to Layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Thumb from "@/components/Thumb";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +32,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased p-0`}
       >
         <Navbar />
+        <Image width={1920} height={1000} src="/bg1.jpg" alt="" className="lg:block w-[100%] border-b-8 border-[#009688]"/>
+        {/* <Thumb /> */}
         {children}
         <Footer />
       </body>
