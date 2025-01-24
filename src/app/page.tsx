88e6feb1f12958e1d1e44e0c820c8d0c1f63b29b
Arthur_Silva <arthursilva.rs1@gmail.com>
2025-01-24 16:19:43 -0300

    :sparkles: feat: change homePage with videos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,72 @@
+import Container from "@/components/Container";
+import VideoCard from "@/components/VideoCard";
+
+type VideoCardProps = {
+  title: string;
+  description: string;
+  image: string;
+  video: string;
+  size: VideoCardSize;
+  display: VideoCardDisplay;
+  autoplay: boolean;
+}
+
+enum VideoCardSize {
+  big = 500,
+  md = 300
+}
+
+enum VideoCardDisplay {
+  right = 1,
+  left = 2,
+  center = 3,
+}
+
 export default function Home() {
+
+  let video = {
+    title: "Me conheça",
+    description: "Prazer, me chamo Juliano Machado, sou Ortopedista e Traumatologista.",
+    image: "Thumbnail",
+    video: "https://www.youtube.com/embed/kNKCqeDnOis?si=8dHZiiZS83nvaCAA?rel=0&autoplay=1&mute=1",
+    size: VideoCardSize.big,
+    display: VideoCardDisplay.right,
+    autoplay: true,
+    muted: true,
+  }
+
+  let video2 = {
+    title: "Me conheça",
+    description: "Prazer, me chamo Juliano Machado, sou Ortopedista e Traumatologista.",
+    image: "Thumbnail",
+    video: "https://www.youtube.com/embed/kNKCqeDnOis?si=8dHZiiZS83nvaCAA?rel=0&autoplay=1&mute=1",
+    size: VideoCardSize.big,
+    display: VideoCardDisplay.center,
+    autoplay: true,
+    muted: true,
+  }
+
+  let video3 = {
+    title: "Me conheça",
+    description: "Prazer, me chamo Juliano Machado, sou Ortopedista e Traumatologista.",
+    image: "Thumbnail",
+    video: "https://www.youtube.com/embed/kNKCqeDnOis?si=8dHZiiZS83nvaCAA?rel=0&autoplay=1&mute=1",
+    size: VideoCardSize.big,
+    display: VideoCardDisplay.left,
+    autoplay: true,
+    muted: true,
+  }
+
   return (
-    <div className="h-[1500px]">
+    <div className="min-h-[1500px]">
       <main>
-        <h1>Bem-vindo ao site!</h1>
-        <h1>Bem-vindo ao site!</h1>
-        <h1>Bem-vindo ao site!</h1>
-        <h1>Bem-vindo ao site!</h1>
-        <h1>Bem-vindo ao site!</h1>
-        <h1>Bem-vindo ao site!</h1>
-        <h1>Bem-vindo ao site!</h1>
-        <h1>Bem-vindo ao site!</h1>
-        <h1>Bem-vindo ao site!</h1>
-        <h1>Bem-vindo ao site!</h1>
-        <h1>Bem-vindo ao site!</h1>
-        <h1>Bem-vindo ao site!</h1>
-        <h1>Bem-vindo ao site!</h1>
+        <Container>
+          <VideoCard props={video}/>
+          <VideoCard props={video2}/>
+          <VideoCard props={video3}/>
+          <VideoCard props={video2}/>
+          <VideoCard props={video}/>
+        </Container>
       </main>
     </div>
   );
